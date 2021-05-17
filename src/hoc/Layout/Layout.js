29c90ce366792aaadf7d287c692651460d147b69
Aminul Momin <A.Momin.NYC@gmail.com>
2021-05-17 03:49:26 -0400

    Add sketeton of BurgerBuilder component

--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -1,5 +1,7 @@
 import React from "react";
 import Aux from "../Aux/Aux";
+import BurgerBuilder from "../../containers/BurgerBuilder/BurgerBuilder";
+import classes from "./Layout.module.css";
 
 const layout = (props) => {
   return (
@@ -7,7 +9,9 @@ const layout = (props) => {
       <h3 style={{ color: "red" }}>Slot for Sidebar</h3>
       <h3 style={{ color: "red" }}>Slot for BackDrop</h3>
 
-      <main>{props.children}</main>
+      <main className={classes.container}>
+        <BurgerBuilder />
+      </main>
     </Aux>
   );
 };
